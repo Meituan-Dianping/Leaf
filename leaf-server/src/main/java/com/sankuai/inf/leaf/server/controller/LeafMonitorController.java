70035afff46ed9d4d7368488dfef5fd3d488bbd8
thelight1 <han122655904@163.com>
2019-09-06 20:27:27 +0800

    decode snowflake id

--- leaf-server/src/main/java/com/sankuai/inf/leaf/server/controller/LeafMonitorController.java
+++ leaf-server/src/main/java/com/sankuai/inf/leaf/server/controller/LeafMonitorController.java
@@ -10,8 +10,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -65,4 +70,34 @@ public class LeafMonitorController {
         return "db";
     }
 
+    /**
+     * the output is like this:
+     * {
+     *   "timestamp": "1567733700834(2019-09-06 09:35:00.834)",
+     *   "sequenceId": "3448",
+     *   "workerId": "39"
+     * }
+     */
+    @RequestMapping(value = "decodeSnowflakeId")
+    @ResponseBody
+    public Map<String, String> decodeSnowflakeId(@RequestParam("snowflakeId") String snowflakeIdStr) {
+        Map<String, String> map = new HashMap<>();
+        try {
+            long snowflakeId = Long.parseLong(snowflakeIdStr);
+
+            long originTimestamp = (snowflakeId >> 22) + 1288834974657L;
+            Date date = new Date(originTimestamp);
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+            map.put("timestamp", String.valueOf(originTimestamp) + "(" + sdf.format(date) + ")");
+
+            long workerId = (snowflakeId >> 12) ^ (snowflakeId >> 22 << 10);
+            map.put("workerId", String.valueOf(workerId));
+
+            long sequence = snowflakeId ^ (snowflakeId >> 12 << 12);
+            map.put("sequenceId", String.valueOf(sequence));
+        } catch (NumberFormatException e) {
+            map.put("errorMsg", "snowflake Id反解析发生异常!");
+        }
+        return map;
+    }
 }
