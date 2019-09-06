51d6fdcc68cf67661887e28185a7f01fc61c0220
thelight1 <122655904@qq.com>
2019-09-06 20:33:22 +0800

    decode snowflake id

--- leaf-server/src/main/java/com/sankuai/inf/leaf/server/Constants.java
+++ leaf-server/src/main/java/com/sankuai/inf/leaf/server/Constants.java
@@ -5,6 +5,7 @@ public class Constants {
     public static final String LEAF_JDBC_URL = "leaf.jdbc.url";
     public static final String LEAF_JDBC_USERNAME = "leaf.jdbc.username";
     public static final String LEAF_JDBC_PASSWORD = "leaf.jdbc.password";
+
     public static final String LEAF_SNOWFLAKE_ENABLE = "leaf.snowflake.enable";
     public static final String LEAF_SNOWFLAKE_PORT = "leaf.snowflake.port";
     public static final String LEAF_SNOWFLAKE_ZK_ADDRESS = "leaf.snowflake.zk.address";
