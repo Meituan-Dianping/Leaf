48de3348e9790f5b3b4b3ad0c68562cf773eebda
凉之 <zhaodong.xzd@alibaba-inc.com>
2019-10-09 23:11:12 +0800

    init support springboot

--- leaf-plugin/leaf-boot-starter/src/main/java/com.sankuai.inf.leaf.plugin/LeafSpringBootStarterAutoConfigure.java
+++ leaf-plugin/leaf-boot-starter/src/main/java/com.sankuai.inf.leaf.plugin/LeafSpringBootStarterAutoConfigure.java
@@ -1,5 +1,8 @@
 package com.sankuai.inf.leaf.plugin;
 
+import com.sankuai.inf.leaf.server.exception.InitException;
+import com.sankuai.inf.leaf.server.service.SegmentService;
+import com.sankuai.inf.leaf.server.service.SnowflakeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -18,15 +21,14 @@ public class LeafSpringBootStarterAutoConfigure {
     private LeafSpringBootProperties properties;
     @Bean
     @ConditionalOnProperty(prefix = "leaf.segment",value = "enable",matchIfMissing = false)
-    public Void initLeafSegmentStarter(){
-
+    public SegmentService initLeafSegmentStarter(){
         return null;
     }
 
     @Bean
     @ConditionalOnProperty(prefix = "leaf.snowflake",value = "enable",matchIfMissing = false)
-    public Void initLeafSnowflakeStarter(){
-
-        return null;
+    public SnowflakeService initLeafSnowflakeStarter() throws InitException {
+        SnowflakeService snowflakeService =new SnowflakeService();
+        return snowflakeService;
     }
 }
