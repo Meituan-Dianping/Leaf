bb71c063e379356aee4f4cbac610488d549847f6
Not Found <577528249@qq.com>
2020-05-04 13:24:52 +0800

    fix timestamp over

---
+++ leaf-core/src/main/java/com/sankuai/inf/leaf/snowflake/exception/OverMaxTimeStampException.java
@@ -0,0 +1,7 @@
+package com.sankuai.inf.leaf.snowflake.exception;
+
+public class OverMaxTimeStampException extends RuntimeException {
+    public OverMaxTimeStampException(String msg){
+        super(msg);
+    }
+}
