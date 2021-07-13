82542ed1ff2b08bdd5ecbb100a7f10cbffb6ca8d
liuqiang <liuqiang@oigcn.com>
2021-07-13 11:55:02 +0800

    去掉leaf.properties，使用starter注解运行

--- leaf-server/src/main/java/com/sankuai/inf/leaf/server/LeafServerApplication.java
+++ leaf-server/src/main/java/com/sankuai/inf/leaf/server/LeafServerApplication.java
@@ -1,11 +1,13 @@
 package com.sankuai.inf.leaf.server;
 
+import com.sankuai.inf.leaf.plugin.annotation.EnableLeafServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource("classpath:leaf-application.xml")
+@EnableLeafServer
+//@ImportResource("classpath:leaf-application.xml")
 public class LeafServerApplication {
 
 	public static void main(String[] args) {
